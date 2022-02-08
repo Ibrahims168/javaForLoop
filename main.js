746ea17afe6c839ffb9bbfce3b0d16440d4ce8fa
ibrahims168 <ibrahh881@gmail.com>
2022-02-08 16:08:48 +0200

    remove und

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, corrupti. Odit consequatur quasi dolorem eum nulla quas, perferendis ab nobis, quae saepe animi distinctio molestiae atque adipisci neque aliquam!.'
-for (let i = 0; str[i] !=undefined; i++) {
+for (let i = 0; str[i]; i++) {
     console.log(str[i]);
     
 }
