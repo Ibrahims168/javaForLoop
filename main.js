a4da84b79b73769fd6783f6711757a4819a0d67b
ibrahims168 <ibrahh881@gmail.com>
2022-02-08 17:19:06 +0200

    newupdate

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const str = 'ibrahim sharif safadi 1999'
-let spaceContainer = 0
-for (let i = 0; str[i]; i++) {
-    if (str[i] ==  ' '){
-        spaceContainer++
-    }
-}
-    console.log(spaceContainer);  
+// const str = 'ibrahim sharif safadi 1999'
+// let spaceContainer = 0
+// for (let i = 0; str[i]; i++) {
+//     if (str[i] ==  ' '){
+//         spaceContainer++
+//     }
+// }
+//     console.log(spaceContainer);  
 
     
 
@@ -24,3 +24,29 @@ for (let i = 0; str[i]; i++) {
 // } else {
 //     console.log('falsy');
 // }
+
+
+
+
+
+// const str = 'Ibrahim Sharif Id Number Phone Email Fax A'
+// let counter = 0 
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] == 'A') {
+//        counter++ 
+//     }
+// }
+// console.log(counter);
+
+
+
+
+
+const str = 'Ibrahim Sharif Id Number Phone Email Fax A'
+let counter = 0 
+    for (let i = 0; str[i]; i++) {
+        if (str.charCodeAt(i) <= '90' && str.charCodeAt(i) >= '65') {
+          counter ++
+        }
+    }
+console.log(counter);
