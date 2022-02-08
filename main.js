5dbeeff6a95eed7ec48118c5c8ab24ec031f8adc
ibrahims168 <ibrahh881@gmail.com>
2022-02-08 16:26:56 +0200

    newupdate

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, corrupti. Odit consequatur quasi dolorem eum nulla quas, perferendis ab nobis, quae saepe animi distinctio molestiae atque adipisci neque aliquam!.'
 for (let i = 0; str[i]; i++) {
-    console.log(str[i]);
-    
+    console.log(str[i]);  
+}
+let char = 'undefined'
+undefined // falsy
+NaN // falsy
+null // falsy
+
+let sum = char + 5
+
+console.log(sum);
+if (sum) {
+    console.log('truthy');
+} else {
+    console.log('falsy');
 }
