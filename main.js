b03945c88c15a0792ca02f5ab9a6faa1f3d67542
ibrahims168 <ibrahh881@gmail.com>
2022-02-08 16:00:27 +0200

    javaclass2

---
+++ main.js
@@ -0,0 +1,5 @@
+const str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, corrupti. Odit consequatur quasi dolorem eum nulla quas, perferendis ab nobis, quae saepe animi distinctio molestiae atque adipisci neque aliquam!.'
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    
+}
